fa24a55411de37fa055f24e74752cf3aeeec26da
Rafayel Mkrtchyan <rafamian@berkeley.edu>
2015-03-18 15:41:05 -0700

    Update translate.c

--- src/translate.c
+++ src/translate.c
@@ -63,7 +63,6 @@ unsigned write_pass_one(FILE* output, const char* name, char** args, int num_arg
 }
 
 /* Writes the instruction in hexadecimal format to OUTPUT during pass #2.
-   
    NAME is the name of the instruction, ARGS is an array of the arguments, and
    NUM_ARGS specifies the number of items in ARGS. 
 
