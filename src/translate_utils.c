132a088bb7876a4371ba830ed8e0ebf3acb3f49b
Rafayel Mkrtchyan <rafamian@berkeley.edu>
2015-03-21 00:54:34 -0700

    Update translate_utils.c

--- src/translate_utils.c
+++ src/translate_utils.c
@@ -45,7 +45,7 @@ int is_valid_label(const char* str) {
    decimal or hexadecimal format. It is also possible that the input is not
    a valid number.  
 
-   You should store the result into the location that OUTPUT points to. The 
+   This stores the result into the location that OUTPUT points to. The 
    function returns 0 if the conversion proceeded without errors, or -1 if an 
    error occurred.
  */
